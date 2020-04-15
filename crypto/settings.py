0583aca7d200f86490c1f4e32940e82dc8874ea3
tigerzh@office <2>
2020-04-15 17:27:08 +0800

    Site updated: 2020-04-15 17:27:08

--- crypto/settings.py
+++ crypto/settings.py
@@ -78,17 +78,17 @@ DATABASES = {
         # 需要安装数据库包 mysqlclient
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crypto',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT': '3306',
-        # 'CONN_MAX_AGE':None,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8',
-        }
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'crypto',
+        # 'HOST': '127.0.0.1',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'PORT': '3306',
+        # # 'CONN_MAX_AGE':None,
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #     'charset': 'utf8',
+        # }
     }
 }
 
